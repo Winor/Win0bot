328cc0e840a41aedfde422d69f21022db7ea61ff
Winor <or123123@gmail.com>
2021-08-27 16:04:00 +0300

    New cup game script

---
+++ src/robot_modules/cupgame.ts
@@ -0,0 +1,43 @@
+import { Interaction, Message, MessageButton } from 'discord.js';
+import Commend from '../Commend'
+import type w0bMessage from '../discord/adapter'
+
+export default class FindTheBall extends Commend {
+constructor() {
+    super({
+        name: 'cupgame',
+        cmd: ['cupgame', 'cg'],
+        hear: [],
+        globalHear: [],
+        platform: "discord",
+        lvl: 0
+    })
+}
+
+async run(msg: w0bMessage): Promise<void> {
+    try {
+        msg.defer()
+        const ball = Math.floor(Math.random() * 3)
+        
+        const btns: MessageButton[] = []
+        for (let i = 0; i < 3; i++) {       
+            btns.push(msg.createBtn(`${i+1}`))
+        }
+        const row = msg.createRow(btns)
+
+        const newMsg = await msg.followUp({ content: 'Where is the ball ?', components: [row] }) as Message | Interaction
+        const input = await msg.collector(newMsg)
+        if (input?.componentType === "BUTTON") {
+            if(input.customId === ball.toString()) {
+                await input.update({ content:'You are right !', components: [] })
+            } else {
+                await input.update({ content:`Better luck next time ! \nThe ball was at cup number ${ball+1}`, components: [] })
+            }
+        }
+
+        
+    } catch (e) {
+        console.log(e)
+    }   
+}
+}
