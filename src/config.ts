6df00d22bae3c98538ddb3e861a36398ed0d6d45
Winor <or123123@gmail.com>
2022-01-18 03:31:18 +0200

    new config entries

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,19 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-function getEnv(entry: string, parse?: boolean) {
+interface GetEnvFlags {
+  entry: string
+  parse?: boolean
+  required?: boolean
+}
+
+function getEnv({entry, parse = false, required = true}: GetEnvFlags) {
   const env = process.env[entry]
   if (env === undefined) {
+    if (required) {
     throw new Error(`Missing confing entry for ${entry}`)
+  }
+  return undefined
   }
   if (parse) {
     return JSON.parse(env)
@@ -12,13 +21,26 @@ function getEnv(entry: string, parse?: boolean) {
   return env
 }
 
+function getUrl() {
+  const dburl = getEnv({ entry: 'DATABASE_URL', required: false})
+  if (dburl) {
+    return dburl
+  }
+  const dbHost = getEnv({ entry: 'POSTGRESQL_URL'})
+  const dbUser = getEnv({ entry: 'POSTGRES_USER'})
+  const dbPassw = getEnv({ entry: 'POSTGRESQL_PASSWORD'})
+  const dbPort = getEnv({ entry: 'POSTGRESQL_PORT_NUMBER'})
+  return `postgresql://${dbUser}:${dbPassw}@${dbHost}:${dbPort}/mydb?schema=public`
+}
+
 export default {
-    discordToken: getEnv('discordToken'),
-    telegramToken: getEnv('telegramToken'),
-    admins: getEnv('admins', true),
-    mods: getEnv('mods', true),
-    prefix: getEnv('prefix'),
-    voiceTextChnList: getEnv('voiceTextChnList', true),
-    voiceChannleJoinMsg: getEnv('voiceChannleJoinMsg'),
-    effectiveRoles: getEnv('effectiveRoles', true)
+    discordToken: getEnv({ entry: 'discordToken'}),
+    telegramToken: getEnv({ entry: 'telegramToken'}),
+    admins: getEnv({ entry: 'admins', parse: true}),
+    mods: getEnv({ entry: 'mods', parse: true}),
+    prefix: getEnv({ entry: 'prefix'}),
+    voiceTextChnList: getEnv({ entry: 'voiceTextChnList', parse: true}),
+    voiceChannleJoinMsg: getEnv({ entry: 'voiceChannleJoinMsg'}),
+    effectiveRoles: getEnv({ entry: 'effectiveRoles', parse: true}),
+    dbUrl: getUrl()
   }
