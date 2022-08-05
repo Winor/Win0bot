a45a6f1402be729db1657da4b7296bd22897047f
Winor <or123123@gmail.com>
2022-08-05 17:30:47 +0300

    Make interactive and & added permissions check

--- src/robot_modules/updateDiscord.ts
+++ src/robot_modules/updateDiscord.ts
@@ -1,12 +1,14 @@
 import Commend from '../Commend'
 import type w0bMessage from '../adapters/discord/adapter'
 import {commandData} from '../listenerMap'
+import { GuildMember } from 'discord.js'
 
 export default class UpdateDiscord extends Commend {
 constructor() {
     super({
         name: 'update',
         cmd: ['update'],
+        description: 'Update Discord commands list',
         hear: [],
         globalHear: [],
         platform: "discord",
@@ -16,9 +18,17 @@ constructor() {
 
 async run(msg: w0bMessage): Promise<void> {
     try {
-        const guild = msg.raw.guild
-        console.log(commandData)
-        guild?.commands.set(commandData)
+        await msg.defer({ephemeral: true})
+        if (msg.raw.member instanceof GuildMember) {
+            if (msg.raw.member.permissions.has('Administrator')){
+                const guild = msg.raw.guild
+                console.log(commandData)
+                guild?.commands.set(commandData)
+                msg.followUp({content: `Done, pushed ${commandData.length} commands.`, ephemeral: true})
+                return
+            }
+            msg.followUp({content: 'You do not have permissions', ephemeral: true})
+        }
         
     } catch (e) {
         console.log(e)
